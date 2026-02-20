dbcbac5deaa5096dc8dbf83c3e82da1dfefbd809
bootplus <bootplus@naver.com>
2026-02-20 23:26:38 +0900

    dark mode switch 추가 및 main vue package 이동

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,74 @@
 <script setup lang="ts">
-import { Menubar, MenubarMenu, MenubarTrigger, MenubarContent, MenubarItem } from '@/components/ui/menubar'
-import { CommandDialog, CommandInput, CommandList, CommandEmpty, CommandGroup, CommandItem } from '@/components/ui/command'
-import { useMagicKeys, useColorMode } from '@vueuse/core'
-import { ref, watch } from 'vue'
-import Main from '@views/Main.vue';
+import { Menubar, MenubarMenu, MenubarTrigger, MenubarContent, MenubarItem } from '@/components/ui/menubar';
+import { CommandDialog, CommandInput, CommandList, CommandEmpty, CommandGroup, CommandItem } from '@/components/ui/command';
+import { Switch } from "@components/ui/switch";
+import { SunIcon, MoonIcon } from "lucide-vue-next";
+import { useMagicKeys, useColorMode, useDark, useToggle } from '@vueuse/core';
+import { ref, watch } from 'vue';
+import { Main } from '@views/main';
 
-const mode = useColorMode({
+useColorMode({
   selector: 'html', // html 태그에 'dark' 클래스를 입힘
   attribute: 'class',
   initialValue: 'dark'
 });
 
-mode.value = 'dark';
-
-const { ctrl, k } = useMagicKeys()
-const open = ref(false)
+const { ctrl, k } = useMagicKeys();
+const open = ref(false);
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
 
 // Ctrl + K 단축키 감지
 watch([ctrl, k], ([ctrlValue, kValue]) => {
   if (ctrlValue && kValue) open.value = true
-})
+});
 </script>
 
 <template>
   <div class="flex flex-col h-screen overflow-hidden">
     <header
       class="fixed start-0 top-0 flex h-10 w-full items-center justify-between border-b bg-background/80 backdrop-blur-md select-none"
-      style="-webkit-app-region: drag"
-    >
-      <div class="flex items-center px-4" style="-webkit-app-region: no-drag">
-        <Menubar class="border-none bg-transparent shadow-none">
-          <MenubarMenu>
-            <MenubarTrigger class="text-xs bg-background/80 font-bold">File</MenubarTrigger>
-            <MenubarContent>
-              <MenubarItem class="text-xs">New Window</MenubarItem>
-              <MenubarItem class="text-xs">Settings</MenubarItem>
-            </MenubarContent>
-          </MenubarMenu>
-        </Menubar>
+      style="-webkit-app-region: drag">
+
+      <div class="flex w-full items-center justify-between">
+
+        <div class="flex items-center px-4" style="-webkit-app-region: no-drag">
+          <Menubar class="border-none bg-transparent shadow-none">
+            <MenubarMenu>
+              <MenubarTrigger class="text-xs bg-background/80 font-bold">File</MenubarTrigger>
+              <MenubarContent>
+                <MenubarItem class="text-xs">New Window</MenubarItem>
+                <MenubarItem class="text-xs">Settings</MenubarItem>
+              </MenubarContent>
+            </MenubarMenu>
+          </Menubar>
+        </div>
+
+        <div
+          class="flex h-7 w-64 items-center justify-between rounded-md border bg-muted px-3 text-xs text-muted-foreground cursor-pointer hover:bg-accent"
+          style="-webkit-app-region: no-drag"
+          @click="open = true">
+          <span>Search commands...</span>
+          <kbd class="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-background px-1.5 font-mono text-[10px] font-medium opacity-100">
+            <span class="text-xs">⌘</span>K
+          </kbd>
+        </div>
+
+        <Switch :model-value="isDark" @update:model-value="toggleDark" style="-webkit-app-region: no-drag">
+          <template #thumb>
+            <div class="flex h-full w-full items-center justify-center">
+              <MoonIcon v-if="isDark" class="h-3 w-3 text-yellow-500" />
+              <SunIcon v-else class="h-3 w-3 text-gray-500" />
+            </div>
+          </template>
+        </Switch>
+
       </div>
 
-      <div
-        class="flex h-7 w-64 items-center justify-between rounded-md border bg-muted px-3 text-xs text-muted-foreground cursor-pointer hover:bg-accent"
-        style="-webkit-app-region: no-drag"
-        @click="open = true"
-      >
-        <span>Search commands...</span>
-        <kbd class="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-background px-1.5 font-mono text-[10px] font-medium opacity-100">
-          <span class="text-xs">⌘</span>K
-        </kbd>
-      </div>
-
-      <div class="w-35"></div>
+      <div class="w-45"></div>
     </header>
 
-    <Main />
+    <Main></Main>
 
     <CommandDialog v-model:open="open">
       <CommandInput placeholder="Type a command or search..." />
--- src/views/Main.vue
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-</script>
-
-<template>
-  <div class="flex-1 p-4">
-
-  </div>
-</template>
