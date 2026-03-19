a09becee52eb16a8c95346a9dfe1024d4587f2dc
bootplus <bootplus@naver.com>
2026-03-19 19:43:34 +0900

    main tab 제거

--- src/views/main/Main.vue
+++ src/views/main/Main.vue
@@ -1,27 +1,10 @@
 <script setup lang="ts">
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@components/ui/tabs";
 import MainBreadcrumb from "./MainBreadcrumb.vue";
 </script>
 
 <template>
-  <div class="flex-1 p-4 mt-10 overflow-auto">
-    <Tabs default-value="user">
-      <TabsList>
-        <TabsTrigger value="user" class="text-xs w-78">사용자 정의</TabsTrigger>
-        <TabsTrigger value="biz" class="text-xs">사내 업무</TabsTrigger>
-        <TabsTrigger value="helper" class="text-xs">업무 도우미</TabsTrigger>
-        <TabsTrigger value="edu" class="text-xs">교육</TabsTrigger>
-      </TabsList>
-      <TabsContent value="user" class="text-xs flex flex-col gap-4">
-        <MainBreadcrumb />
-        <router-view />
-      </TabsContent>
-      <TabsContent value="biz" class="text-xs">
-      </TabsContent>
-      <TabsContent value="helper" class="text-xs">
-      </TabsContent>
-      <TabsContent value="edu" class="text-xs">
-      </TabsContent>
-    </Tabs>
+  <div class="flex flex-col p-4 mt-10 gap-4 overflow-auto">
+    <MainBreadcrumb />
+    <router-view />
   </div>
 </template>
